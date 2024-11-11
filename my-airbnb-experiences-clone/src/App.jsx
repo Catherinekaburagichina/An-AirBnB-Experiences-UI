583d90d5781af4fec51d83a514226ea8fcffcc04
Catherine Gichina <catherinegichinaa@gmail.com>
2024-11-11 19:48:50 +0300

    Implement hero section, and navbar with basic styling and alignment

---
+++ my-airbnb-experiences-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import NavigationBar from "./NavigationBar"
+import Hero from "./Hero"
+function App() {
+  
+
+  return (
+    <>
+      <NavigationBar></NavigationBar>
+      <Hero></Hero>
+    </>
+  )
+}
+
+export default App
